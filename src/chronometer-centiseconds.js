b003994a6f6dfef2197646990ff353518cdcf579
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-04-05 02:30:31 +0200

    done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,71 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime += 1;
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime / 100);
+    if (!this.currentTime) {
+      return 0;
+    } else {
+      return Math.floor(seconds / 60);
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime / 100);
+    if (!this.currentTime) {
+      return 0;
+    } else if (seconds >= 60) {
+      return seconds % 60;
+    } else {
+      return seconds;
+    }
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    if (!this.currentTime) {
+      return 0;
+    } else if (this.currentTime >= 100) {
+      return this.currentTime % 100;
+    } else {
+      return this.currentTime;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value.toString().slice(0, 2);
+    }
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ":" +
+      this.computeTwoDigitNumber(this.getSeconds()) +
+      "." +
+      this.computeTwoDigitNumber(this.getCentiseconds())
+    );
   }
 }
--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (!this.currentTime) {
+      return 0;
+    } else {
+      return Math.floor(this.currentTime / 60);
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (!this.currentTime) {
+      return 0;
+    } else if (this.currentTime >= 60) {
+      return this.currentTime % 60;
+    } else {
+      return this.currentTime;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value.toString().slice(0,2);
+    }
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ":" +
+      this.computeTwoDigitNumber(this.getSeconds())
+    );
   }
 }
